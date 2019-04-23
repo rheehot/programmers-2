990046c46670aeca992280f552b50c68c50d54c0
janghyoseog <bestdevhyo1225@gmail.com>
2019-04-23 21:53:45 +0900

    소수 만들기

--- 2017_summer_coding/make_prime_number.cpp
+++ 2017_summer_coding/make_prime_number.cpp
@@ -1,49 +1,32 @@
-// Summer Coding - 배달
-#include <iostream>
 #include <vector>
-#include <queue>
+#include <cmath>
+#include <iostream>
 using namespace std;
 
-vector<int> dijkstra(vector<vector<pair<int, int> > >& adj, int N) {
-    const int INF = 0x7FFFFFFF;
-    vector<int> dist(N+1, INF);
-    dist[1] = 0;
-    
-    priority_queue<pair<int, int> > pq;
-    pq.push(make_pair(0, 1));
-    while (!pq.empty()) {
-        int curCost = -pq.top().first;
-        int cur = pq.top().second;
-        pq.pop();
-        
-        if (dist[cur] < curCost) continue;
-        
-        for (int i = 0; i < adj[cur].size(); i++) {
-            int next = adj[cur][i].first;
-            int nextCost = adj[cur][i].second + curCost;
-            
-            if (dist[next] > nextCost) {
-                dist[next] = nextCost;
-                pq.push(make_pair(-nextCost, next));
-            }
+int solution(vector<int> nums) {
+    const int limit = 50 * 1000;
+    vector<bool> isPrimeNum(limit+1, true);
+    for (int i = 2; i < sqrt(limit); i++) {
+        if (!isPrimeNum[i]) continue;
+        for (int j = i*i; j <= limit; j+=i) {
+            if (!isPrimeNum[j]) continue;
+            isPrimeNum[j] = false;
         }
     }
     
-    return dist;
-}
-
-int solution(int N, vector<vector<int> > road, int K) {
     int answer = 0;
-    
-    vector<vector<pair<int, int> > > adj(N+1);
-    for (int i = 0; i < road.size(); i++) {
-        adj[road[i][0]].push_back(make_pair(road[i][1], road[i][2]));
-        adj[road[i][1]].push_back(make_pair(road[i][0], road[i][2]));
-    }
-    
-    vector<int> dist = dijkstra(adj, N);
-    for (int i = 1; i < dist.size(); i++) {
-        if (dist[i] <= K) answer++;
+    int n = (int)nums.size();
+    for (int i = 0; i < n; i++) {
+        int sum = nums[i];
+        for (int j = i+1; j < n; j++) {
+            sum += nums[j];
+            for (int k = j+1; k < n; k++) {
+                sum += nums[k];
+                if (isPrimeNum[sum]) answer++;
+                sum -= nums[k];
+            }
+            sum -= nums[j];
+        }
     }
     
     return answer;
@@ -53,34 +36,8 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
-    vector<vector<int > > road(5);
-    road[0].push_back(1);
-    road[0].push_back(2);
-    road[0].push_back(1);
-    
-    road[1].push_back(2);
-    road[1].push_back(3);
-    road[1].push_back(3);
-    
-    road[2].push_back(5);
-    road[2].push_back(2);
-    road[2].push_back(2);
-    
-    road[3].push_back(1);
-    road[3].push_back(4);
-    road[3].push_back(2);
-    
-    road[4].push_back(5);
-    road[4].push_back(3);
-    road[4].push_back(1);
-    
-    road[4].push_back(5);
-    road[4].push_back(4);
-    road[4].push_back(2);
-    
-    
-    int answer = solution(5, road, 3);
-    cout << answer << '\n';
+    vector<int> nums = {1, 2, 7, 6, 4};
+    cout << solution(nums) << '\n';
     return 0;
 }
 
