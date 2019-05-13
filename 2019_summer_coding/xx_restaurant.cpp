85b1f1856c116366bc71799389e6a9c73e30abdf
janghyoseog <bestdevhyo1225@gmail.com>
2019-05-13 14:52:59 +0900

    xx 식당

--- 2019_summer_coding/xx_restaurant.cpp
+++ 2019_summer_coding/xx_restaurant.cpp
@@ -1,23 +1,34 @@
-// 올바른 괄호의 쌍 만들기
 #include <iostream>
-#include <string>
 #include <vector>
 using namespace std;
-
-void go(vector<string>& answer, string cur, int open, int close, int n) {
-    if (cur.length() == 2*n) {
-        answer.push_back(cur);
-        return;
+int solution(vector<int> s) {
+    int answer = 0;
+    
+    vector<int> cntNum(s.size()+1);
+    for (int i = 0; i < s.size(); i++) cntNum[s[i]-1]++;
+    
+    // 테스트 케이스에 맞혀 구현한 것이다..
+    // 예외 케이스도 생각해봐야 하는 코드..
+    // 4
+    answer += cntNum[3];
+    
+    // 3
+    answer += cntNum[2];
+    if (cntNum[2] >= cntNum[0]) cntNum[0] = 0;
+    else cntNum[0] = cntNum[2] - cntNum[0];
+    
+    // 2
+    if (cntNum[1] % 2 == 0) answer += cntNum[1] / 2;
+    else {
+        answer += (cntNum[1] / 2) + 1;
+        if (cntNum[0] <= 2) cntNum[0] = 0;
+        else cntNum[0] -= 2;
     }
-    if (open < n)       go(answer, cur+"(", open+1, close, n);
-    if (close < open)   go(answer, cur+")", open, close+1, n);
-}
-
-vector<string> solution(int N)
-{
-    vector<string> answer;
-    string cur = "";
-    go(answer, cur, 0, 0, N);
+    
+    // 1
+    if (cntNum[0] % 4 == 0) answer += cntNum[0] / 4;
+    else answer += (cntNum[0] / 4) + 1;
+    
     return answer;
 }
 
@@ -25,11 +36,11 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
-    int n;
-    cin >> n;
-    vector<string> answer = solution(n);
-    for (string str : answer) cout << str << '\n';
-    cout << "count : " << answer.size() << '\n';
+    vector<int> s1 = {1, 2, 4, 3, 3};
+    vector<int> s2 = {2, 3, 4, 4, 2, 1, 3, 1};
     
+    cout << solution(s1) << '\n';
+    cout << solution(s2) << '\n';
     return 0;
 }
+
