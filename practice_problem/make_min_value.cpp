f70679632cd943f56b45eeceb1b3e3bb19cc8e6c
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-12 13:46:05 +0900

    최솟값 만들기

---
+++ practice_problem/make_min_value.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), greater<int>());
+    
+    int answer = 0;
+    for (int i = 0; i < A.size(); i++) answer += (A[i] * B[i]);
+    return answer;
+}
