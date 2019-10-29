c88ed5586f908285c56c0be162fb3746910320f2
janghyoseog <bestdevhyo1225@gmail.com>
2019-10-29 17:30:02 +0900

    베스트 앨범

--- practice_problem/best_album.cpp
+++ practice_problem/best_album.cpp
@@ -30,9 +30,11 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<pair<string, int> > tempInfo;
     for (auto p : hashMap) {
         string genre = p.first;
+        
         vector<pair<int, int> > info = p.second;
         
         int totalPlay = 0;
+        
         for (auto p2 : info) totalPlay += p2.second;
         
         tempInfo.push_back(make_pair(genre, totalPlay));
@@ -41,6 +43,8 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
     sort(tempInfo.begin(), tempInfo.end(), compareTotalPlay);
     
     // 장르 내에서 재생 횟수가 높은 순서대로 정렬하고 나서 정답을 구하기..
+    // 정답을 구할 때 주의할 점!
+    // 장르 내에 음악이 없을수도 있고, 1개만 있을수도 있다는 점을 고려하기
     vector<int> answer;
     
     for (auto p : tempInfo) {
@@ -50,7 +54,7 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
         
         // 정렬 -> 고유 번호, 재생 횟수
         sort(temptempInfo.begin(), temptempInfo.end(), comparePlay);
-        
+    
         int tempSize = (int)temptempInfo.size();
         
         if (tempSize > 1) tempSize = 2;
@@ -67,7 +71,7 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
-    vector<int> answer1 = solution({ "classic", "pop", "classic", "classic", "pop" }, { 500, 600, 150, 800, 2500 });
+    vector<int> answer1 = solution({"classic", "pop", "classic", "classic", "pop"}, {500, 600, 150, 800, 2500});
     
     for (int value : answer1) {
         cout << value << " ";
@@ -75,7 +79,7 @@ int main() {
     
     cout << '\n';
     
-    vector<int> answer2 = solution({ "classic", "pop", "classic", "pop", "classic", "classic" }, {400, 600, 150, 2500, 500, 500});
+    vector<int> answer2 = solution({"classic", "pop", "classic", "pop", "classic", "classic"}, {400, 600, 150, 2500, 500, 500});
     
     for (int value : answer2) {
         cout << value << " ";
