af908e36d3b6576ec049e74be5c1b1651861d88e
janghyoseog <bestdevhyo1225@gmail.com>
2019-12-02 15:35:43 +0900

    뉴스 클러스터링

---
+++ 2018_kakao_blind_recruitment/news_clustering.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+vector<string> split(string str) {
+    vector<string> temp;
+    
+    for (int i = 0; i < str.length()-1; i++) {
+        string tempStr = str.substr(i, 2);
+        
+        // 영문에서 소문자, 대문자가 아닌 경우
+        bool check = true;
+        for (char ch : tempStr) {
+            if (('a' > ch || ch > 'z') && ('A' > ch || ch > 'Z')) {
+                check = false;
+                break;
+            }
+        }
+        
+        if (!check) continue;
+        
+        temp.push_back(tempStr);
+    }
+    
+    // 소문자를 영문자로 변환
+    vector<string> result;
+    for (string s : temp) {
+        transform(s.begin(), s.end(), s.begin(), ::toupper);
+        result.push_back(s);
+    }
+    
+    return result;
+}
+
+int solution(string str1, string str2) {
+    vector<string> splitStr1 = split(str1);
+    vector<string> splitStr2 = split(str2);
+    
+    if (splitStr1.empty() && splitStr2.empty()) {
+        return 65536;
+    }
+    
+    set<string> words;
+    
+    for (string s : splitStr1) words.insert(s);
+    for (string s : splitStr2) words.insert(s);
+    
+    int intersectionCnt = 0;
+    int unionCnt = 0;
+
+    for (string s : words) {
+        int cnt1 = 0;
+        for (string s1 : splitStr1) {
+            if (s == s1) cnt1++;
+        }
+        
+        int cnt2 = 0;
+        for (string s2 : splitStr2) {
+            if (s == s2) cnt2++;
+        }
+        
+        int minCnt = min(cnt1, cnt2);
+        int maxCnt = max(cnt1, cnt2);
+        
+        intersectionCnt += minCnt;
+        unionCnt += maxCnt;
+    }
+    
+    double answer = (double)intersectionCnt / (double)unionCnt;
+    
+    return answer * 65536;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cout << solution("FRANCE", "french") << '\n';
+    cout << solution("handshake", "shake hands") << '\n';
+    cout << solution("aa1+aa2", "AAAA12") << '\n';
+    cout << solution("E=M*C^2", "e=m*c^2") << '\n';
+    
+    return 0;
+}
