2978879d31acca058f496efaccb43e8d6e795cd9
janghyoseog <bestdevhyo1225@gmail.com>
2020-04-12 15:08:10 +0900

    자물쇠와 열쇠

---
+++ 2020_kakao_blind_recruitment/lock_and_key.js
@@ -0,0 +1,77 @@
+function rotated(key) {
+  const keySize = key.length;
+  const rotatedKey = Array.from(Array(keySize), () => Array());
+  for (let r = 0; r < keySize; r++) {
+    for (let c = 0; c < keySize; c++) {
+      rotatedKey[c][keySize - 1 - r] = key[r][c];
+    }
+  }
+  return rotatedKey;
+}
+
+function isUnlock(key, lock, r, c) {
+  const keySize = key.length;
+  const lockSize = lock.length;
+  const boardSize = lockSize + keySize * 2 - 2;
+  const board = Array.from(Array(boardSize), () => Array());
+
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board.length; c++) {
+      const lR = r - (keySize - 1);
+      const lC = c - (keySize - 1);
+
+      if (0 <= lR && lR < lockSize && 0 <= lC && lC < lockSize) {
+        board[r][c] = lock[lR][lC];
+        continue;
+      }
+
+      board[r][c] = 0;
+    }
+  }
+
+  for (let bR = r; bR < r + keySize; bR++) {
+    for (let bC = c; bC < c + keySize; bC++) {
+      board[bR][bC] += key[bR - r][bC - c];
+    }
+  }
+
+  for (let lR = keySize - 1; lR <= boardSize - keySize; lR++) {
+    for (let lC = keySize - 1; lC <= boardSize - keySize; lC++) {
+      if (board[lR][lC] !== 1) return false;
+    }
+  }
+
+  return true;
+}
+
+function solution(key, lock) {
+  const keySize = key.length;
+  const lockSize = lock.length;
+  const boardSize = lockSize + keySize * 2 - 2;
+  for (let rotate = 0; rotate < 4; rotate++) {
+    for (let r = 0; r <= boardSize - keySize; r++) {
+      for (let c = 0; c <= boardSize - keySize; c++) {
+        if (isUnlock(key, lock, r, c)) {
+          return true;
+        }
+      }
+    }
+    key = rotated(key);
+  }
+
+  return false;
+}
+
+const key = [
+  [0, 0, 0],
+  [1, 0, 0],
+  [0, 1, 1],
+];
+
+const lock = [
+  [1, 1, 1],
+  [1, 1, 0],
+  [1, 0, 1],
+];
+
+console.log(solution(key, lock));
