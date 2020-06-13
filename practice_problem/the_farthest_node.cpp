24153e0fb68081ca983b8336ae4f69ad702f3f46
janghyoseog <bestdevhyo1225@gmail.com>
2020-06-13 20:32:53 +0900

    가장 먼 노드

---
+++ practice_problem/the_farthest_node.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+const int NODE_LIMIT_COUNT = 20001;
+
+int solution(int n, vector<vector<int>> edge) {
+    
+    vector<int> adj[NODE_LIMIT_COUNT];
+    
+    for (int i = 0; i < edge.size(); i++) {
+        int u = edge[i][0];
+        int v = edge[i][1];
+        
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    vector<int> dist = vector<int>(n+1, -1);
+    queue<int> q;
+    
+    dist[1] = 0;
+    q.push(1);
+    
+    int maxDist = 0;
+    
+    while (!q.empty()) {
+        int current = q.front();
+        q.pop();
+        
+        for (int i = 0; i < adj[current].size(); i++) {
+            int next = adj[current][i];
+            
+            if (dist[next] == -1) {
+                dist[next] = dist[current] + 1;
+                maxDist = max(maxDist, dist[next]);
+                q.push(next);
+            }
+        }
+    }
+    
+    int answer = 0;
+    
+    for (int i = 1; i <= n; i++) {
+        if (maxDist == dist[i]) answer++;
+    }
+    
+    return answer;
+}
